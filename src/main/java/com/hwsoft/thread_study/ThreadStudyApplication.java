04b3b0b211c4c2eae4b4b800e545bf17eb1ad111
zhaomy <zmyluckboy@163.com>
2020-05-08 23:16:16 +0800

    first commit

---
+++ src/main/java/com/hwsoft/thread_study/ThreadStudyApplication.java
@@ -0,0 +1,13 @@
+package com.hwsoft.thread_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreadStudyApplication.class, args);
+    }
+
+}
